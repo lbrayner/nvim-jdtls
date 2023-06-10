002bed6b9f99122b82a3ca49f750a528422d8db0
lbrayner <lbrayner@users.noreply.github.com>
2023-06-10 18:46:06 -0300

    java_type_hierarchy: empty hierarchy exti condition after pop

--- lua/jdtls.lua
+++ lua/jdtls.lua
@@ -879,12 +879,14 @@ function M.java_type_hierarchy(reuse_win, on_list)
       return execute_command(resolve_command(parent), resolve_handler)
     end
 
-    if #hierarchy == 0 then return print('Type hierarchy: no results.') end
-
-    local root = hierarchy[#hierarchy]
-    if root.detail..'.'..root.name == 'java.lang.Object' then
-      table.remove(hierarchy) -- Pop the top
+    if #hierarchy > 0 then
+      local root = hierarchy[#hierarchy]
+      if root.detail..'.'..root.name == 'java.lang.Object' then
+        table.remove(hierarchy) -- Pop the top
+      end
     end
+
+    if #hierarchy == 0 then return print('Type hierarchy: no results.') end
 
     local locations = vim.tbl_map(function(parent)
       return { uri = parent.uri, range = parent.selectionRange }
